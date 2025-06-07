9ef819dfeddb3a8f738bd0645a3ecacc1174b8c4
MathiasPinon51 <pinonmathias51@gmail.com>
2025-06-07 15:23:19 +0200

    Adding comments in getAnimeData

--- script/dataManagement.R
+++ script/dataManagement.R
@@ -2,11 +2,13 @@
 library(tidyverse)
 library(tm)
 
-
+# This function get the anime data and clean it.
+# @return anime data.frame
 getAnimeData <- function(){
   #Getting anime data  
   anime <- read.csv("./data/anime.csv")
   
+  #Paste all relevant data and remove unnecessary punctuation
   anime$dataForIA <- paste(anime$genre,anime$type,anime$episodes) %>%
     removePunctuation()
   return(anime)
