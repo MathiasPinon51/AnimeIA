aa89354e2886f1d3ccc0abf14eb85294cee389af
MathiasPinon51 <pinonmathias51@gmail.com>
2025-06-08 18:32:09 +0200

    Edit getAnimeData function to remove the plurials words

--- script/dataManagement.R
+++ script/dataManagement.R
@@ -1,6 +1,7 @@
 
 library(tidyverse)
 library(tm)
+library(textstem)
 
 # This function get the anime data and clean it.
 # @return anime data.frame
@@ -8,9 +9,16 @@ getAnimeData <- function(){
   #Getting anime data  
   anime <- read.csv("./data/anime.csv")
   
-  #Paste all relevant data and remove unnecessary punctuation
-  anime$dataForIA <- paste(anime$genre,anime$type,anime$episodes) %>%
-    removePunctuation()
+  #Paste all relevant data and clean it
+  anime$dataForIA <- paste(anime$Genre, anime$Type, anime$numEpisode, anime$KeyWord) %>%
+    tolower() %>%
+    removePunctuation() %>%
+    removeWords(stopwords("en")) %>%
+    stripWhitespace() %>%
+    strsplit(split = " ") %>%
+    lapply(lemmatize_words) %>%
+    sapply(paste, collapse = " ")
+  
   return(anime)
 }
   
